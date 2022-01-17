0f3eaea50b9c8d18a1f6061326432adcae96b7b9
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2022-01-18 01:42:04 +0300

    квадратик ходит но лагает

--- level1.py
+++ level1.py
@@ -1,7 +1,8 @@
 import pygame
-from settings import tile_size
+from settings import tile_size, screen_width
 from tile import Tile
-from player import Player
+from player import *
+
 
 class Level1:
     def __init__(self, level_data, surface):
@@ -14,7 +15,7 @@ class Level1:
         self.player = pygame.sprite.GroupSingle()
         for row_i, row in enumerate(layout):
             for col_i, col in enumerate(row):
-                #print(f'{row_i},{col_i}:{col}')
+                # print(f'{row_i},{col_i}:{col}')
                 x, y = col_i * tile_size, row_i * tile_size
 
                 if col == '1':
@@ -24,23 +25,48 @@ class Level1:
                 if col == 'p':  # p это player
                     player_sprite = Player((x, y))
                     self.player.add(player_sprite)
+
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        axis_x = player.axis.x
+        if player_x < screen_width / 4 and axis_x < 0:
+            self.world_shift = 5
+            player.speed = 0
+        elif player_x > screen_width - (screen_width / 4) and axis_x > 0:
+            self.world_shift = -5
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 5
+
+    def horizontal_collision(self):
+        player = self.player.sprite
+        player.rect.x += player.axis.x * player.speed
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.axis.x < 0:
+                    player.rect.left = sprite.rect.right
+                elif player.axis.x > 0:
+                    player.rect.right = sprite.rect.left
+
+    def vertical_collision(self):
+        player = self.player.sprite
+        self.make_gravity()
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.axis.y > 0:
+                    player.rect.bottom = sprite.rect.top
+                    player.axis = 0
+                elif player.axis.y < 0:
+                    player.rect.top = sprite.rect.bottom
+
     def run(self):
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+
         self.player.draw(self.display_surface)
         self.player.update()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        self.horizontal_collision()
+        self.vertical_collision()
+        self.scroll_x()
